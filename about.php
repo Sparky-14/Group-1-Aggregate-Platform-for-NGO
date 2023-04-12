cadad93dac0064fb2c2bbd09bece5c1614e5045c
Sparky-14 <108272964+Sparky-14@users.noreply.github.com>
2023-04-12 15:01:35 +0530

    Update about.php

--- about.php
+++ about.php
@@ -9,28 +9,28 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
+
   <link href="assets/img/icon.png" rel="icon">
   <link href="assets/img/touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
+  
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
+  
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
+  
   <link href="assets/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
+ 
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
@@ -46,7 +46,7 @@
     </ul>
     </div>
   </header>
-  <!-- End Header -->
+  
 <div>
     <section id="about" class="about">
       <div class="container">
@@ -64,7 +64,7 @@
       </div>
     </section>
   </div>
-    <!-- ======= Counts Section ======= -->
+   
     <section id="counts" class="counts">
       <div class="container">
 
@@ -94,8 +94,7 @@
 
       </div>
     </section>
-    <!-- End Counts Section -->
-    <!-- ======= Cta Section ======= -->
+
     <section id="cta" class="cta">
       <div class="container">
 
@@ -107,8 +106,7 @@
 
       </div>
     </section>
-    <!-- End Cta Section -->
-    <!-- ======= Contact Section ======= -->
+    
     <section id="contact" class="contact">
       <div class="container">
 
@@ -177,12 +175,9 @@
 
       </div>
     </section>
-    <!-- End Contact Section -->
-
+    
   </main>
-  <!-- End #main -->
-
-  <!-- ======= Footer ======= -->
+  
   <footer id="footer">
 
     <div class="footer-top">
@@ -220,18 +215,15 @@
         &copy; Copyright <strong><span>eNGO</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/enno-free-simple-bootstrap-template/ -->
+        
         Designed by <a>Team 1</a>
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
+  
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
@@ -239,7 +231,7 @@
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
+  
   <script src="assets/js/main.js"></script>
 
 </body>
